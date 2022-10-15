cfd7f93ed285cb14ced9270fb33f6efc39d36670
Anderson Rodrigues Dias <anderson33rd@hotmail.com>
2022-10-15 13:43:12 -0300

    Gerar mvn

--- epictaskapi/src/test/java/br/com/fiap/epictaskapi/EpictaskapiApplicationTests.java
+++ epictaskapi/src/test/java/br/com/fiap/epictaskapi/EpictaskapiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EpictaskapiApplicationTests {
+class RaposymApplicationTests {
 
 	@Test
 	void contextLoads() {
